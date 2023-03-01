4a460ba05e3d68b7853d7014a709753061dfe4e1
topstar210 <happycamper0210@gmail.com>
2023-03-01 15:21:42 +0900

    clean db query added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from "mongoose";
+
 // controllers
 import users from '../controllers/user.js';
 // middlewares
@@ -7,6 +9,16 @@ import { encode } from '../middlewares/jwt.js';
 const router = express.Router();
 
 router
-  .post('/login/:userId', encode, (req, res, next) => { });
+  .post('/login/:userId', encode, (req, res, next) => { })
+  // for dev
+  .get('/clean_db', (req, res) => { 
+    mongoose.connection.db.dropCollection('answers', function(err, result) {
+      console.log("successfully drop the answers collection")
+    });
+    mongoose.connection.db.dropCollection('game', function(err, result) {
+      console.log("successfully drop the game collection")
+    });
+    res.send("successfully drop the collections");
+  })
 
 export default router;
